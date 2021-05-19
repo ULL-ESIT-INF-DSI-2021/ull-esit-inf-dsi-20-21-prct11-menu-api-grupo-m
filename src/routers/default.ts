f93870ede586b1db6b241f71ceadae0d596cf826
Eduardo Pérez Suárez <alu0100890174@ull.edu.es>
2021-05-19 19:50:50 +0100

    heroku

--- src/routers/default.ts
+++ src/routers/default.ts
@@ -2,6 +2,9 @@ import * as express from 'express';
 
 export const defaultRouter = express.Router();
 
+/**
+ * Default Router
+ */
 defaultRouter.all('*', (_, res) => {
   res.status(501).send();
 });
